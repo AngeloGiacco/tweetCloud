9da386a09b9ee6d9f9141ae47f5df33b778f5d97
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-04-12 21:47:57 +0100

    Create page.py

---
+++ page.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
