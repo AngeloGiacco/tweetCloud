469df89a980cca614fb3f30b42dea821e320559a
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-29 16:21:41 +0100

    connect to API

--- source/tweet_collector.py
+++ source/tweet_collector.py
@@ -1,2 +1,6 @@
 import tweepy
-from credentials.py import *
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+api = tweepy.API(auth)
