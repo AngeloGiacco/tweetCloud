a37f12fb109d5e7485e4d9a4981493412b1ce8dd
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-29 16:29:52 +0100

    update way the text is displayed

--- source/tweet_collector.py
+++ source/tweet_collector.py
@@ -13,6 +13,6 @@ api = tweepy.API(auth)
 
 def user_tweet(twitter_handle):
     statuses = api.user_timeline(screen_name=twitter_handle, count=2, tweet_mode="extended")
-    return statuses
+    return statuses.full_text.split()
 
 print(user_tweet("oundleschool"))
