451c0dc661a7899dbfc42b679622698c188f71b4
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-29 16:22:49 +0100

    Update tweet_collector.py

--- source/tweet_collector.py
+++ source/tweet_collector.py
@@ -4,3 +4,9 @@ from credentials import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth)
+
+def user_tweet(twitter_handle):
+    statuses = api.user_timeline(screen_name=twitter_handle, count=2, tweet_mode="extended")
+    return statuses
+
+print(user_tweet("oundleschool"))
