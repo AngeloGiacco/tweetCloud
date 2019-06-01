848c3d8dd9d34c4efe6efefc79c01ceb43026721
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-06-01 17:54:06 +0100

    print tweets

--- source/page.py
+++ source/page.py
@@ -1,4 +1,31 @@
 from flask import Flask, request, render_template
+try:
+    import tweepy
+except ImportError:
+    print("ENSURE YOU HAVE INSTALLED tweepy")
+try:
+    from credentials import *
+except ImportError:
+    print("ENSURE YOU HAVE CREATED credentials.py IN THE SAME FOLDER")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+word_cloud_lst = []
+tweet_lst = []
+
+def user_tweet(twitter_handle):
+    tweets = api.user_timeline(screen_name=twitter_handle, count=10, tweet_mode="extended")
+    clean = []
+    for tweet in tweets:
+        for word in tweet.full_text.split():
+            if 'https:' in word or 'http:' in word or 'www' in word or '.com' in word:
+                continue
+            else:
+                word_cloud_lst.append(word)
+                clean.append(word)
+        tweet_lst.append(" ".join(clean))
+        clean = []
 
 app = Flask(__name__)
 
@@ -8,9 +35,12 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['account']
-    processed_text = text.upper()
-    return processed_text
+    global tweet_lst
+    account = request.form['account']
+    user_tweet(account)
+    tweet_text = ("<br><br>").join(tweet_lst)
+    tweet_lst = []
+    return tweet_text
 
 if __name__ == '__main__':
     app.run(debug=True)
--- source/tweet_collector.py
+++ source/tweet_collector.py
@@ -3,6 +3,10 @@ try:
 except ImportError:
     print("ENSURE YOU HAVE INSTALLED tweepy")
 try:
+    from credentials import *
+except ImportError:
+    print("ENSURE YOU HAVE CREATED credentials.py IN THE SAME FOLDER")
+try:
     from wordcloud import WordCloud, STOPWORDS
 except ImportError:
     print("ENSURE YOU HAVE INSTALLED wordcloud")
@@ -18,40 +22,35 @@ try:
     import numpy as np
 except ImportError:
     print("ENSURE YOU HAVE INSTALLED numpy")
-try:
-    from credentials import *
-except ImportError:
-    print("ENSURE YOU HAVE CREATED credentials.py IN THE SAME FOLDER")
+
 import re
 from PIL import Image
 import urllib
 
-class tweetCollector:
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+word_cloud_lst = []
 
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    word_cloud_lst = []
-
-    def user_tweet(twitter_handle):
-        tweets = api.user_timeline(screen_name=twitter_handle, count=200, tweet_mode="extended")
+def user_tweet(twitter_handle):
+    tweets = api.user_timeline(screen_name=twitter_handle, count=200, tweet_mode="extended")
+    clean = []
+    for tweet in tweets:
+        for word in tweet.full_text.split():
+            if 'https:' in word or 'http:' in word or 'www' in word or '.com' in word:
+                continue
+            else:
+                word_cloud_lst.append(word)
+                clean.append(word)
         clean = []
-        for tweet in tweets:
-            for word in tweet.full_text.split():
-                if 'https:' in word or 'http:' in word or 'www' in word or '.com' in word:
-                    continue
-                else:
-                    word_cloud_lst.append(word)
-                    clean.append(word)
-            clean = []
 
-    def generate_wordcloud(words, mask):
-        word_cloud = WordCloud(width = 512, height = 512, background_color='white', stopwords=STOPWORDS, mask=mask).generate(words)
-        plt.figure(figsize=(10,8),facecolor = 'white', edgecolor='blue')
-        plt.imshow(word_cloud)
-        plt.axis('off')
-        plt.tight_layout(pad=0)
-        plt.show()
+def generate_wordcloud(words, mask):
+    word_cloud = WordCloud(width = 512, height = 512, background_color='white', stopwords=STOPWORDS, mask=mask).generate(words)
+    plt.figure(figsize=(10,8),facecolor = 'white', edgecolor='blue')
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    plt.show()
 """
 if __name__ == '__main__':
 
